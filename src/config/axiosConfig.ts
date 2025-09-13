874cee3de10d65c8ce8977ee9468d9cef8324219
hamza-yameen <hamzayameen3000@gmail.com>
2025-09-13 21:19:59 +0500

    added new designs

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -9,7 +9,7 @@ interface CustomAxiosRequestConfig extends AxiosRequestConfig {
 
 // Create axios instance with default configuration
 const api: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://www.refinedreport.com/backend/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://www.refinedreport.com/backend/",
   headers: {
     'Content-Type': 'application/json',
   },
